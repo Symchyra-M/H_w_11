b9514512acbd7371a8a86bf61f50047a497d11f8
Symchyra-M <137912223+Symchyra-M@users.noreply.github.com>
2023-10-04 17:21:51 +0200

    .

--- main.py
+++ main.py
@@ -4,130 +4,108 @@ from datetime import datetime
 
 class Field:
     def __init__(self, value):
-        self._value = None
-        self.value = value
+        self._value = value
 
     @property
     def value(self):
         return self._value
 
     @value.setter
-    def value(self, value):
-        self._value = value
+    def value(self, new_value):
+        self._value = new_value
+
+    def is_valid(self, value):
+        pass
 
     def __str__(self):
-        return str(self._value)
+        return str(self.value)
 
 
 class Name(Field):
-    @Field.value.setter
-    def value(self, name):
-        self._value = name
+    pass
 
 
 class Phone(Field):
-    @Field.value.setter
-    def value(self, phone):
-        if len(phone) != 10 or not phone.isdigit():
-            raise ValueError('Enter correct phone')
-        self._value = phone
+    def __init__(self, value):
+        super().__init__(value)
+        if not self.is_valid(value):
+            raise ValueError('Error, the number must have 10 digits.')
+
+    def is_valid(self, value):
+        return len(value) == 10 and self.value.isdigit()
 
 
-class Birthday(Field):  # Тут birthday це об'єкт datetime
-    @Field.value.setter
-    def value(self, birthday):
-        if isinstance(birthday, datetime):
-            self._value = birthday
-        else:
-            raise ValueError('Enter correct birthday')
+class Birthday(Field):
+    def is_valid(self, value):
+        super().__init__(value)
+        try:
+            datetime.strptime(value, '%d-%m-%Y')
+        except ValueError:
+            print('Invalid birthday format. Try DD-MM-YYYY.')
 
 
 class Record:
     def __init__(self, name, birthday=None):
         self.name = Name(name)
         self.phones = []
-        self.birthday = birthday  # Тут birthday це екземпляр класу Birthday
+        self.birthday = Birthday(birthday) if birthday else None
 
     def add_phone(self, phone):
-        valid_phone = Phone(phone)
-        if valid_phone.value is not None:
-            self.phones.append(valid_phone)
-        else:
-            return 'Phone is not valid'
+        phone = Phone(phone)
+        self.phones.append(phone)
 
     def remove_phone(self, phone):
-        valid_phone = Phone(phone)
-        phone_to_remove = None
         for ph in self.phones:
-            if ph.value == valid_phone.value:
-                phone_to_remove = ph
-                break
-        if phone_to_remove is not None:
-            self.phones.remove(phone_to_remove)
-        else:
-            return 'This contact don`t have this phone'
+            if phone == ph:
+                self.phones.remove(phone)
+                return self.phones
+        raise ValueError('Invalid number.')
 
-    def edit_phone(self, old_phone_value, new_phone_value):
-        old_phone_index = None
-        for i, ph in enumerate(self.phones):
-            if ph.value == old_phone_value:
-                old_phone_index = i
-                break
-        if old_phone_index is not None:
-            new_phone = Phone(new_phone_value)
-            if new_phone.value is not None:
-                self.phones[old_phone_index] = new_phone
-            else:
-                return 'New phone is not valid'
-        else:
-            raise ValueError('Phone not found')
+    def edit_phone(self, old_phone, edit_phone):
+        for ph in self.phones:
+            if ph.value == old_phone:
+                ph.value = edit_phone
+                return ph.value
+        raise ValueError('Invalid number.')
 
     def find_phone(self, phone):
-        valid_phone = Phone(phone)
-        for ph in self.phones:
-            if ph.value == valid_phone.value:
-                return ph
+        for p in self.phones:
+            if p.value == phone:
+                return p
         return None
 
-    def days_to_birthday(self):
-        if self.birthday:
-            today_day = datetime.today()
-            current_year = today_day.year
-            contact_birthday = self.birthday.value
-            contact_birthday = contact_birthday.replace(year=current_year)
-            if contact_birthday < today_day:
-                contact_birthday = contact_birthday.replace(year=current_year + 1)
-            result = contact_birthday - today_day
-            return result.days
-        else:
-            return 'No birthday info for this contact'
-
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}."
+
+    def days_to_birthday(self):
+        if not self.birthday:
+            return None
+
+        today = datetime.now()
+        birthday_date = datetime(self.birthday.value.day, self.birthday.value.month, self.birthday.value.year)
+
+        if today > birthday_date:
+            next_birthday = datetime(self.birthday.value.day, self.birthday.value.month, self.birthday.value.year + 1)
+        else:
+            next_birthday = birthday_date
+
+        days_left = (next_birthday - today).days
+        return days_left
 
 
 class AddressBook(UserDict):
     def add_record(self, record):
-        if isinstance(record, Record):
-            self.data[record.name.value] = record
-        else:
-            raise ValueError("Here you can add only records")
+        self.data[record.name.value] = record
 
     def find(self, name):
-        return self.data.get(name, None)
+        if name in self.data:
+            return self.data[name]
 
     def delete(self, name):
         if name in self.data:
-            self.data.pop(name)
-        else:
-            print(f"This contact don`t exist")
+            del self.data[name]
 
-    def iterator(self, n):
-        index = 0
-        not_fitting = len(self.data) % n
-        while index < len(self.data):
-            try:
-                yield dict(list(self.data.items())[index:index + n])
-                index += n
-            except IndexError:
-                yield dict(list(self.data.items())[index:index + not_fitting])
+    def iterator(self, num_of_records=5):
+        records = list(self.data.values())
+        for i in range(0, len(records), num_of_records):
+            yield records[i:i + num_of_records]
